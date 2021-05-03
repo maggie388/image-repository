c08bb5488d860a488d5cbf5cbaccb7ae60b0ba63
maggie388 <rodriguesmargarida@hotmail.com>
2021-05-03 11:11:17 -0400

    Refactor Header nav list items and add upload image button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,16 +5,27 @@ import { Link } from 'react-router-dom';
 const { REACT_APP_SERVER_URL: SERVER_URL } = process.env;
 
 const Header = ({ isLoggedIn, setShowLoginModal }) => {
+    const PrivateMenuItems = () => {
+        return (
+            <>
+                <li className='header__list-item'>Upload an Image</li>
+                <li className='header__list-item'><a className='header__button' href={`${SERVER_URL}/auth/logout`}>Sign Out</a></li>
+            </>
+        );
+    };
+    
+    const PublicMenuItems = () => {
+        return (
+            <li className='header__list-item'><button className='header__button' onClick={() => setShowLoginModal(true)}>Login</button></li>
+        )
+    }
+    
     return (
         <header className='header'>
             <h1><Link className='header__title' to='/'>IMGrepo</Link></h1>
             <nav>
                 <ul className='header__list'>
-                    <li className='header__list-item'>
-                        {isLoggedIn ?
-                        <a className='header__button' href={`${SERVER_URL}/auth/logout`}>Sign Out</a> :
-                        <button className='header__button' onClick={() => setShowLoginModal(true)}>Login</button>}
-                    </li>
+                    {isLoggedIn ? PrivateMenuItems() : PublicMenuItems()}
                 </ul>
             </nav>
         </header>
