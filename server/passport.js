089af69dfc42f3a4759b1170252d4dbadcab99cd
maggie388 <rodriguesmargarida@hotmail.com>
2021-05-03 10:41:17 -0400

    Refactor environment varaiable imports in passport.js file

--- server/passport.js
+++ server/passport.js
@@ -4,10 +4,12 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const Users = require('./models/usersTable');
 
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, SERVER_URL} = process.env;
+
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${process.env.SERVER_URL}/api/auth/return`
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: `${SERVER_URL}/api/auth/return`
   },
   (accessToken, refreshToken, profile, done) => {
       console.log(profile);
