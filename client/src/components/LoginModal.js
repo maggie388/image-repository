23c820657907839fffcf499dd417865088bcfc50
maggie388 <rodriguesmargarida@hotmail.com>
2021-05-03 10:48:17 -0400

    Refactor env value imports in LoginModal

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -3,12 +3,15 @@ import './LoginModal.scss';
 // assets
 import googleLogo from '../assets/icons/google-logo.png';
 
+// variables
+const { REACT_APP_SERVER_URL: SERVER_URL } = process.env;
+
 const LoginModal = () => {
     return (
         <div className='login-modal'>
             <h3 className='login-modal__title'>Login</h3>
             <p className='login-modal__phrase'>Welcome Back</p>
-            <a href={`${process.env.REACT_APP_SERVER_URL}/api/auth/login/google`} className='login-modal__button'>
+            <a href={`${SERVER_URL}/api/auth/login/google`} className='login-modal__button'>
                 <img className='login-modal__google-logo' src={googleLogo} alt='Google logo' />
                 Login with Google
             </a>
